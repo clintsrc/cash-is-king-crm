87b93907968581d227535c3623ac1c47758e0caf
ethanboud <ethanboud@gmail.com>
2025-04-01 22:46:13 -0600

    addition of nearly complete typedefs file

--- server/src/schemas/typeDefs.ts
+++ server/src/schemas/typeDefs.ts
@@ -21,6 +21,57 @@ const typeDefs = `
     password: String!
   }
 
+  type Order {
+    _id: ID
+    first_name: String
+    last_name: String
+    phone_number: Number
+    email: String
+    createdAt: Date
+    start_date: Date
+    end_date: Date
+    atms: Number
+    description: String
+    status: [Status]!
+    address: [Address]!
+  }
+
+  input OrderInput {
+    first_name: String!
+    last_name: String!
+    phone_number: Number!
+    email: String!
+    start_date: Date!
+    end_date: Date!
+    atms: Number!
+    description: String!
+    status: [Status]!
+    ################## status may need to be updated so that it defaults to pending upon initial creation. need to see if this is handled via resolver - ethan
+    address: [Address]!
+  }
+
+  type Status {
+    pending: String
+    scheduled: String
+    completed: String
+    denied: String
+  }
+
+  type Address {
+    street: String
+    city: String
+    state: String
+    zip: Number
+    createdAt: Date
+  }
+
+  input AddressInput {
+    street: String
+    city: String
+    state: String
+    zip: Number
+  }
+
   type Auth {
     token: ID!
     user: User
@@ -30,6 +81,8 @@ const typeDefs = `
   type Query {
     user(username: String!): User
     me: User
+    getOrderByStatus(status: [Status]!): Order
+
   }
 
   ##############  Mutations
@@ -39,6 +92,17 @@ const typeDefs = `
 
     # Login user and return JWT token (signin)
     loginUser(email: String!, password: String!): Auth
+
+    # create an order
+    order(input: OrderInput!): Order
+
+    ############### create address VERY UNSURE, NEEDS LOOKING INTO - ethan
+    addAddress(OrderId:ID, input: AddressInput!): Order
+
+    ############### update status VERY UNSURE, NEEDS LOOKING INTO - ethan
+    changeStatus(OrderId:ID, status: String!): Order
+
+    ############### may need to look into the addition of more mutations. 
   }
 `;
 

1be54f78e42a5cfe432262f0291278e51cdfd957
Clint Jones <clinton.alan.jones@gmail.com>
2025-04-09 13:09:26 -0700

    Get cypress tests working again

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,26 +1,64 @@
-describe("template spec", () => {
-  context("Navbar", () => {
-    it("should show the login modal when the 'Login / Sign Up' link is clicked", () => {
-      cy.visit("/");
+describe('Admin Login', () => {
+  context('Navbar Login', () => {
+    it("should show the login modal when the 'Login' link is clicked", () => {
+      const email: string = 'admin@test.com';
+      const password: string = 'bootcamp';
 
-      cy.findByRole("button", { name: /login \/ sign up/i })
-        .should("be.visible")
+      cy.visit('/');
+
+      cy.findByRole('button', { name: /login/i })
+        .should('be.visible')
         .click();
 
-      cy.findByRole("dialog", { name: /login/i }).should("be.visible");
-      cy.findByRole("button", { name: /submit/i }).should("be.disabled");
+      cy.findByTestId('email')
+        .should('be.visible')
+        .parents('.modal-dialog')
+        .should('be.visible');
+
+      cy.findByTestId('submit').should('be.visible');
 
       cy.findByTestId('email')
-        .should("be.visible")
-        .type("test@test.com")
-        .should("have.value", "test@test.com");
+        .should('be.visible')
+        .type(email)
+        .should('have.value', email);
 
       cy.findByTestId('password')
-        .should("be.visible")
-        .type("test")
-        .should("have.value", "test");
+        .should('be.visible')
+        .type(password)
+        .should('have.value', password);
 
-      cy.findByTestId('submit').should("be.enabled");
+      cy.findByTestId('submit')
+        .should('be.enabled')
+        .click();
+
+      cy.findByRole('link', { name: /home/i }).should('be.visible');
+
+      // Admin dashboard should be available now
+      cy.findByRole('link', { name: /admin dashboard/i })
+        .should('be.visible')
+        .click();
+
+      cy.findByRole('button', { name: /pending/i })
+        .should('be.visible')
+        .click();
+
+      cy.findByRole('button', { name: /scheduled/i })
+        .should('be.visible')
+        .click();
+
+      cy.findByRole('button', { name: /denied/i })
+        .should('be.visible')
+        .click();
+
+      cy.findByRole('button', { name: /past/i })
+        .should('be.visible')
+        .click();
+
+      cy.findByRole('button', { name: /logout/i })
+        .should('be.visible')
+        .click();
+
+      cy.findByRole('button', { name: /login/i }).should('be.visible');
     });
   });
 });
